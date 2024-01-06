23e5df0959a184cb6b9be2395b2e2def7da24034
Jon Brule <brulejr@gmail.com>
2024-01-06 11:55:35 -0500

    iot-indexer-ms: Removes Kafka dependencies

--- iot-indexer-ms/build.gradle.kts
+++ iot-indexer-ms/build.gradle.kts
@@ -26,23 +26,18 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-websocket")
 
-	implementation("org.springframework.cloud:spring-cloud-stream")
-	implementation("org.springframework.cloud:spring-cloud-stream-binder-kafka-streams")
-
 	implementation("com.google.guava:guava:32.0.0-jre")
 	implementation("io.github.resilience4j:resilience4j-retry:1.7.1")
 	implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
 
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation("org.apache.kafka:kafka-streams")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	implementation("io.github.microutils:kotlin-logging:3.0.4")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
-	testImplementation("org.springframework.cloud:spring-cloud-stream-test-binder")
 }
 
 dependencyManagement {
