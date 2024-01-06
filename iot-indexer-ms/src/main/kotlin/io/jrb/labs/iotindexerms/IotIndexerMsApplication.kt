8a06b5187863a88d1f1fd09765bc3dc53d4eb635
Jon Brule <brulejr@gmail.com>
2024-01-06 13:15:25 -0500

    iot-indexer-ms: Corrects base application class

--- iot-indexer-ms/src/main/kotlin/io/jrb/labs/iotindexerms/IotIndexerMsApplication.kt
+++ iot-indexer-ms/src/main/kotlin/io/jrb/labs/iotindexerms/IotIndexerMsApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class IotListenerMsApplication
+class IotIndexerMsApplication
 
 fun main(args: Array<String>) {
-	runApplication<IotListenerMsApplication>(*args)
+	runApplication<IotIndexerMsApplication>(*args)
 }
